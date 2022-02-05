b18de2c4981e44fdcc739710a68583a32d6bd680
Jessica <1330784336@qq.com>
2022-02-05 19:51:56 +0800

    deploy

---
+++ precache-manifest.df39608538a6ad53d7782ce7d2e25e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a808d3d80ee092a7f5c",
+    "url": "/Accunting-test/css/app.3ceaccd5.css"
+  },
+  {
+    "revision": "88d62420761500ed253d0e52438e66ba",
+    "url": "/Accunting-test/index.html"
+  },
+  {
+    "revision": "6a808d3d80ee092a7f5c",
+    "url": "/Accunting-test/js/app.1d821b97.js"
+  },
+  {
+    "revision": "58fbf838360c163a655e",
+    "url": "/Accunting-test/js/chunk-vendors.c6a96fd6.js"
+  },
+  {
+    "revision": "16eb8ce2633be2fc41d1c683b121e032",
+    "url": "/Accunting-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Accunting-test/robots.txt"
+  }
+]);
